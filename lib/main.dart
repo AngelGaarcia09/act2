71483a4f2a4a4898d1662955af779bad31f9ff43
ANGEL_MISAEL_GARCIA_HERNANDEZ <a.22308051281193@cbtis128.edu.mx>
2025-03-20 18:38:11 +0000

    otra

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,108 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      debugShowCheckedModeBanner: false, // Elimina la etiqueta de "Debug"
       theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
+        appBarTheme: AppBarTheme(
+          color: Colors.pink, // Color rosa para el AppBar
+          elevation: 0, // Elimina la sombra del AppBar
+          iconTheme: IconThemeData(color: Colors.white), // Color de los íconos
+        ),
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Container Garcia 1193', // Título del AppBar
+            style: TextStyle(
+              color: Colors.white, // Color del texto del título
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: 200.0,
+                height:
+                    300.0, // Aumenté la altura para acomodar los 5 elementos
+                padding: EdgeInsets.all(16.0), // Espacio interno
+                decoration: BoxDecoration(
+                  color: Colors.red, // Color de fondo
+                  borderRadius:
+                      BorderRadius.circular(10.0), // Bordes redondeados
+                  boxShadow: [
+                    BoxShadow(
+                      color:
+                          Colors.black.withOpacity(0.5), // Color de la sombra
+                      spreadRadius: 5, // Extensión de la sombra
+                      blurRadius: 7, // Difuminado de la sombra
+                      offset:
+                          Offset(0, 3), // Desplazamiento de la sombra (x, y)
+                    ),
+                  ],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Fecha de nac:',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10), // Espacio entre elementos
+                    Text(
+                      'Edad:',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10), // Espacio entre elementos
+                    Text(
+                      'nombre :',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10), // Espacio entre elementos
+                    Text(
+                      'Direccion:',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10), // Espacio entre elementos
+                    Text(
+                      'Telefono',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
